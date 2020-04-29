42c230ecc5ab6e8243b8a02842dd2717d08f40db
syedmohin <syedmohin@users.noreply.github.com>
2020-04-29 23:05:04 +0530

    First Angular code to push to github From CMDER

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AddDetailsComponent } from './add-details/add-details.component';
+import { EditDetailsComponent } from './edit-details/edit-details.component';
+import { ActivateService } from './services/activate.service';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", redirectTo: "" },
+  { path: "", component: LoginComponent },
+  { path: "home", component: HomeComponent, canActivate: [ActivateService] },
+  { path: "about", component: AboutComponent, canActivate: [ActivateService] },
+  { path: "reg", component: RegisterComponent },
+  { path: "logout", component: LogoutComponent, canActivate: [ActivateService] },
+  { path: "add", component: AddDetailsComponent, canActivate: [ActivateService] },
+  { path: "edit", component: EditDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms'
+import { AppComponent } from './app.component'
+import { HomeComponent } from './home/home.component'
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
+import { HttpClientModule } from '@angular/common/http'
+import { AppRoutingModule } from './app-routing.module'
+import { AboutComponent } from './about/about.component'
+import { LoginComponent } from './login/login.component'
+import { NavbarComponent } from './navbar/navbar.component'
+import { LogoutComponent } from './logout/logout.component'
+import { RegisterComponent } from './register/register.component'
+import { AddDetailsComponent } from './add-details/add-details.component'
+import { EditDetailsComponent } from './edit-details/edit-details.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    LogoutComponent,
+    AboutComponent,
+    AddDetailsComponent,
+    EditDetailsComponent,
+    RegisterComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
